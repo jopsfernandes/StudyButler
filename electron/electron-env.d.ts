955af6429c66f2b4627906742b0431797a963a6d
jopsfernandes <joaopfernandessantos@gmail.com>
2024-03-17 01:33:57 -0400

    feat:add app boilerplate

---
+++ electron/electron-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite-plugin-electron/electron-env" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+  
+    DIST: string
+    /** /dist/ or /public/ */
+    PUBLIC: string
+  }
+}
